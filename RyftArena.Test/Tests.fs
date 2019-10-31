9e2b04ed7a29cf1648cbb43d5483b5d8d8c92e33
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-10-31 15:54:17 -0400

    Add tests for new model

--- RyftArena.Test/Tests.fs
+++ RyftArena.Test/Tests.fs
@@ -1,9 +1,31 @@
 module Tests
 
+open System
 open Xunit
 open FsUnit.Xunit
+open RyftArena
+open Model
 
-[<Fact>]
-let ``true is true``() =
-    true |> should equal true
+module GameSessionTests =
 
+    [<Fact>]
+    let ``Can create a game session`` () =
+        let underlord =
+            { Id = Guid.NewGuid()
+              Name = "Cool Underlord"
+              AttackStyle = Ranged 3<u>
+              Damage = 5
+              MaxHealth = 1000
+              MaxMana = 50 }
+
+        let player =
+            { Id = Guid.NewGuid()
+              PlayerInfo =
+                  Bot
+                    { Id = Guid.NewGuid()
+                      BotName = "Bot Player" }
+              Underlord = underlord }
+
+        let players = [player]
+        let gameSession = GameSession.createGameSession players;
+        (gameSession.Players = players) |> should be True
--- RyftArena/GameSession.fs
+++ RyftArena/GameSession.fs
@@ -1,4 +1,20 @@
 namespace RyftArena
 
+open System
+open Model
+
 module GameSession =
-    ()
+    let createGameSession players =
+        let now = DateTime.UtcNow
+
+        { Id = Guid.NewGuid()
+          Players = players
+          ConnectedPlayers = players
+          DisconnectedPlayers = []
+          PlayerGold = Map.empty
+          PlayerHeroes = Map.empty
+          PlayerItems = Map.empty
+          GameEvents = [InitializeGame, Some now]
+          HeroPool = []
+          ItemPool = []
+          Stage = PreGame }
